eb28983714c78e33b145010205e37d4755e45829
ybchar <ybcha@toptoon.com>
2022-07-29 18:07:58 +0900

    feat: DetailView Keyboard

--- everytime/DetailViewController.swift
+++ everytime/DetailViewController.swift
@@ -76,6 +76,72 @@ final class DetailViewController: UIViewController, UITableViewDataSource, UITab
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(named: "more_title"), style: .plain, target: self, action: nil)
     }
    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+        
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+        
+    
+    
+    private func setupGestureRecognizer() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
+        view.addGestureRecognizer(tap)
+      }
+      
+      @objc func handleTap(_ gesture: UITapGestureRecognizer) {
+        view.endEditing(true)
+      }
+      
+      private func registerForKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector:#selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector:#selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+      }
+      
+      private func unregisterForKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+      }
+      
+      @objc func keyboardWillShow(_ notificatoin: Notification) {
+        let duration = notificatoin.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as! Double
+        let curve = notificatoin.userInfo?[UIResponder.keyboardAnimationCurveUserInfoKey] as! UInt
+        let keyboardSize = (notificatoin.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        let height = keyboardSize.height - view.safeAreaInsets.bottom
+        
+          UIView.animate(withDuration: duration){
+             
+              self.view.layoutIfNeeded()
+          }
+        /*
+            애니메이션 처리
+        */
+      }
+      
+      @objc func keyboardWillHide(_ notificatoin: Notification) {
+        
+        let duration = notificatoin.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as! Double
+        let curve = notificatoin.userInfo?[UIResponder.keyboardAnimationCurveUserInfoKey] as! UInt
+        
+          UIView.animate(withDuration: duration){
+             
+              self.view.layoutIfNeeded()
+          }
+        /*
+            애니메이션 처리
+        */
+      }
+    
+    @objc func viewDidTap(gesture: UITapGestureRecognizer){
+        self.view.endEditing(true)
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.texts.count
     }
--- everytime/ReplyTextView.swift
+++ everytime/ReplyTextView.swift
@@ -117,6 +117,8 @@ final class ReplyTextView: UIView {
         
     }
     
+    
+    
     required init(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
